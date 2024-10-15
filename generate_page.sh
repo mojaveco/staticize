f13bb56ea5f7eacfad05fd6a5f29e35c2d437ae9
JamesC137 <jamesc137@protonmail.com>
2024-10-15 15:46:18 -0700

    manual capitalization added to generate_page.sh

--- generate_page.sh
+++ generate_page.sh
@@ -3,14 +3,17 @@
 # Get the page name from the argument
 PAGE_NAME=$1
 
+# Capitalize the first letter of the page name manually
+CAPITALIZED_PAGE_NAME="$(tr '[:lower:]' '[:upper:]' <<< ${PAGE_NAME:0:1})${PAGE_NAME:1}"
+
 # Create the HTML file in the pages folder
 cat <<EOT > public/pages/$PAGE_NAME.html
-<h1>${PAGE_NAME^}</h1>
-<p>This is the $PAGE_NAME page content.</p>
+<h1>$CAPITALIZED_PAGE_NAME</h1>
+<p>This is the $CAPITALIZED_PAGE_NAME page content.</p>
 EOT
 
 # Update the navigation to include the new page
-sed -i "/<\/ul>/i <li><a href=\"#\" data-page=\"$PAGE_NAME\">${PAGE_NAME^}</a></li>" public/nav.html
+sed -i "/<\/ul>/i <li><a href=\"#\" data-page=\"$PAGE_NAME\">$CAPITALIZED_PAGE_NAME</a></li>" public/nav.html
 
 echo "Page $PAGE_NAME.html created and added to the navigation!"
 
